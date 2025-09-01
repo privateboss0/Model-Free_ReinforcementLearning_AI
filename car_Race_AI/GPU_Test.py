e1e988c11699b489cb248bac79aa382d3250c34d
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2025-09-01 12:42:30 +0000

    update

---
+++ car_Race_AI/GPU_Test.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        
+        print(e)
+
+import gymnasium as gym
+env = gym.make("CarRacing-v3", render_mode="rgb_array")
+print("CarRacing-v3 environment created successfully.")
+env.close()
