2a237d511b031eb0b1b13e14652f0fd42a977a7a
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2025-09-01 12:45:26 +0000

    GPU_Test.py

--- car_Race_AI/GPU_Test.py
+++ car_Race_AI/GPU_Test.py
@@ -4,7 +4,7 @@ print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     try:
-        # Currently, memory growth needs to be the same across GPUs
+        
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
