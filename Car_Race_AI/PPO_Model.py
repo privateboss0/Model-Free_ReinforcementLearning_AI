6c4ebcb8edac8879906a09332f3217bf03fcf84f
Akinkunmi Leo Ola <89046656+privateboss0@users.noreply.github.com>
2025-09-01 18:15:57 +0000

    PPO_Model.py

--- Car_Race_AI/PPO_Model.py
+++ Car_Race_AI/PPO_Model.py
@@ -1,5 +1,4 @@
 #O
-#E
 import gymnasium as gym
 import numpy as np
 from collections import deque
@@ -332,4 +331,5 @@ class PPOAgent:
         print(f"Average modified evaluation reward over {num_episodes} episodes: {np.mean(episode_rewards):.2f}")
         print(f"Average original environment reward over {num_episodes} episodes: {np.mean(episode_original_rewards):.2f}")
         return episode_rewards, episode_original_rewards
+#E
 
